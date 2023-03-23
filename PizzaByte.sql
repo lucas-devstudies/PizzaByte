4865cf458b23577dfb69238f3461d9a5e79fb8b2
Lucas Ferreira <Lucasfdasilva2002@gmail.com>
2023-03-22 20:41:15 -0400

    resolvendo bug de carrinhoProduto

--- PizzaByte.sql
+++ PizzaByte.sql
@@ -10,8 +10,9 @@ CREATE TABLE Produtos(
 	ValorProduto DECIMAL(5,2)
 );
 
-CREATE TABLE Clientes(
+CREATE TABLE Usuarios(
 	IdCliente INT IDENTITY(1,1) PRIMARY KEY,
+	TipoCliente CHAR,
 	NomeCliente VARCHAR(255),
 	TelefoneCliente VARCHAR(16), /*Imaginei que VARCHAR seria melhor por conta das barrinhas ex: (99) 9 9999-9999*/
 	EmailCliente VARCHAR(255),
@@ -19,42 +20,78 @@ CREATE TABLE Clientes(
 	Endereco VARCHAR(255)
 );
 
+CREATE TABLE Entrega(
+	IdEntrega INT IDENTITY(1,1) PRIMARY KEY,
+	NomeMotoboy VARCHAR(255),
+	VeiculoMotoboy VARCHAR(255),
+	TelefoneMotoboy VARCHAR(16),
+	ValorEntrega DECIMAL (18,2),
+	ValorMotoboy DECIMAL(10,2)
+);
+
+
 CREATE TABLE Vendas(
 	IdVenda INT IDENTITY(1,1) PRIMARY KEY,
 	TempoVenda DATETIME, 
 	StatusVenda VARCHAR(255),
 	DataVenda datetime,
 	Preco DECIMAL(5,2),
-	TipoVenda BIT,
+	TipoVenda CHAR,
 	idCliente INT,
-	idProduto INT,
 	idEntrega INT,
-	CONSTRAINT fk_clientes_id FOREIGN KEY (idCliente) 
-	REFERENCES Clientes(IdCliente),
-	CONSTRAINT fk_produtos_id FOREIGN KEY (idProduto)
-	REFERENCES Produtos(IdProduto),
+	CONSTRAINT fk_Usuarios_id FOREIGN KEY (idCliente) 
+	REFERENCES Usuarios(IdCliente),
 	CONSTRAINT fk_entrega_id FOREIGN KEY (idEntrega)
 	REFERENCES Entrega(IdEntrega)
 );
-
-CREATE TABLE Entrega(
-	IdEntrega INT IDENTITY(1,1) PRIMARY KEY,
-	NomeMotoboy VARCHAR(255),
-	VeiculoMotoboy VARCHAR(255),
-	TelefoneMotoboy VARCHAR(16),
-	ValorMotoboy DECIMAL(10,2),
-	ValorEntrega DECIMAL (18,2)
-);
-
-CREATE TABLE ProdutoCliente(
-	IdProduto INT,
+CREATE TABLE Carrinho(
+	IdCarrinho INT PRIMARY KEY,
 	QuantidadePC INT,
-	IdVenda INT,
-	Sabores VARCHAR,
-	Obs VARCHAR,
-	PRIMARY KEY (IdProduto,IdVenda),
-	CONSTRAINT fk_IdProduto FOREIGN KEY (IdProduto)
-	REFERENCES Produtos(IdProduto),
-	CONSTRAINT fk_IdVenda FOREIGN KEY (IdVenda)
-	REFERENCES Vendas(IdVenda)
+	Sabores VARCHAR (255),
+	Observacao VARCHAR (255),
 );
+CREATE TABLE CarrinhoProduto(
+	IdCarrinho int,
+	IdProduto int,
+	PRIMARY KEY (IdCarrinho, IdProduto),
+	FOREIGN KEY(IdProduto)
+	REFERENCES Produtos(IdProduto),
+	FOREIGN KEY (IdCarrinho)
+	REFERENCES Carrinho(IdCarrinho)
+);
+
+drop table Carrinho;
+DROP DATABASE PizzaByte;
+
+INSERT INTO Produtos
+(NomeProduto, CategoriaProduto,
+DescricaoProduto, ValorProduto)
+VALUES 
+('Pizza pequena tradicional', 'Pizza', 'Pizza Pequena até 2 sabores', 18.90);
+
+INSERT INTO Clientes
+(NomeCliente, TelefoneCliente, EmailCliente, SenhaCliente, Endereco)
+VALUES 
+('Taís Waine', '(92) 99111-1111', 'taisWaine@gmail.com', 'senhadatais', 'Avenida Paulista, n57, casa verde');
+
+INSERT INTO Entrega
+(NomeMotoboy, VeiculoMotoboy, TelefoneMotoboy, ValorEntrega, ValorMotoboy)
+VALUES
+('Carlos Alberto', 'Moto Honda pr 147', '(92) 99999-9999', 10.00, 5.00)
+
+INSERT INTO Vendas
+(TempoVenda, StatusVenda, DataVenda, Preco, TipoVenda, idCliente, idProduto, idEntrega)
+VALUES
+('20:00', 'Em andamento', '22/03/2023',  33.90, 0,1,1,1)
+
+INSERT INTO ProdutoCliente
+(IdProduto, QuantidadePC, IdVenda, Sabores, Observacao)
+VALUES 
+(1, 1, 1, 'Pizza Catupiry', 'Sem milho')
+
+SELECT *
+FROM Vendas v
+INNER JOIN Entrega e ON e.IdEntrega IS NOT NULL
+INNER JOIN Clientes c ON c.IdCliente IS NOT NULL
+INNER JOIN Produtos p ON v.IdVenda IS NOT NULL
+
