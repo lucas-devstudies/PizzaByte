f51cc115523f3f0520fc24f2e4388094dad05c8a
Lucas Ferreira <Lucasfdasilva2002@gmail.com>
2023-04-15 18:28:51 -0400

    endpoint de usuário criado

---
+++ Backend/Models/Usuarios.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend.Models
+{
+    public class Usuarios
+    {
+        public int idCliente  { get; set; }  
+        public char tipoCliente { get; set; }   
+        public string nomeCliente { get; set; }
+        public string telefoneCliente { get; set; } 
+    }
+}
