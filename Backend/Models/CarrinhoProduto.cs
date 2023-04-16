c35bd8d6baa204cf6fb3c014fae8c2e33fd591a0
LLOlaBB <lauracbarros@hotmail.com>
2023-04-16 17:33:56 -0300

    CarrinhoProduto endpoint finalizado

--- Backend/Models/CarrinhoProduto.cs
+++ Backend/Models/CarrinhoProduto.cs
@@ -2,5 +2,9 @@
 {
     public class CarrinhoProduto
     {
+        public int IdCarrinho { get; set; }
+        public int IdProduto { get; set; }
+        public int QuantidadePC { get; set; }
+        public string Sabores { get; set; }
     }
 }
